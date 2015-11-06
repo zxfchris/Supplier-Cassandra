d30581ee668195aec46769f3b1fa1248e6ca970b
ligia0119 <ligia0119@gmail.com>
2015-11-06 15:20:56 +0800

    update transaction 6 popular-item transaction

---
+++ src/edu/nus/cs4224/OLComparator.java
@@ -0,0 +1,20 @@
+package edu.nus.cs4224;
+
+import java.util.Comparator;
+
+import edu.nus.cs4224.d8.OrderLine;
+
+public class OLComparator implements Comparator<Object>{
+	@Override
+	public int compare(Object o1, Object o2) {
+		OrderLine ol1 = (OrderLine)o1;
+		OrderLine ol2 = (OrderLine)o2;
+		if (ol1.getOl_quantity().compareTo(ol2.getOl_quantity()) == 1) {
+			return -1;
+		} else if (ol1.getOl_quantity().compareTo(ol2.getOl_quantity()) == -1) {
+			return 1;
+		}
+		return 0;
+	}
+
+}
